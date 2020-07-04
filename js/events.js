834f99d38337d3ef57eecc8b43899594f07d6e91
Thierno Souleymane Diallo <tsd2493@gmail.com>
2020-07-04 23:20:21 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form').on('submit', function(e){
+  $('form').on('submit', function(){
       alert('your form is going to be submitted now.');
 
   })
