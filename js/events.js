b3c940be7fcf194fbf55e7029af91c5aaaf68bcb
Thierno Souleymane Diallo <tsd2493@gmail.com>
2020-07-04 23:24:29 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,12 +15,9 @@ function pressIt(){
 
 function submitIt(){
   $("form").on("submit", function() {
-    if ($( "input:first" ).val() === "correct") {
       alert('Your form is going to be submitted now.');
       return;
-    }
-    alert("you entered the wrong value");
-    return;
+    
   });
 }
 
