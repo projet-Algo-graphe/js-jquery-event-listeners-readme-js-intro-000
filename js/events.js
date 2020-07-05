5179aa9f027f77e307da5ebce0e4cef225e3081d
Thierno Souleymane Diallo <tsd2493@gmail.com>
2020-07-05 00:13:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+$('p').on('click', function(e){
+  alert('Hey!');
+})
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if(e.key === 'G'){
+      alert('You pressed the G key');
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+      alert('Your form is going to be submitted now.');
+      return;
+
+  });
+}
+function frameIt(){
+  $( 'img' ).load(function() {
+    $( this ).addClass( "tasty" );
+});
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+pressIt();
+submitIt();
+frameIt();
 });
