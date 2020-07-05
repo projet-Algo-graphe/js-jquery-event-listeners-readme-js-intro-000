f27972ab6d7d2beb1a797b2e290bbd1864f951ad
Thierno Souleymane Diallo <tsd2493@gmail.com>
2020-07-05 00:10:32 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,7 @@ $('p').on('click', function(e){
 
 function pressIt(){
   $('input').on('keydown', function(e){
-    if(e === "G"){
+    if(e === 'G'){
       alert('You pressed the G key');
     }
   })
